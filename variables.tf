118884a97fb9e19e53da75777d96a9af04bd6c25
tanjaouicloud <100701433+tanjaouicloud@users.noreply.github.com>
2025-10-24 17:35:20 +0200

    Add AWS credentials and region variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
 variable "bucket_name" {
   type = string
 }
+
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-west-1"
+}
